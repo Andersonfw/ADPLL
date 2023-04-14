39d653b1501ea4ab67a119e73c96055786ff0c3d
Andersonfw <andersonfwesch@gmail.com>
2023-04-13 23:47:17 -0300

    Ajustes na PSD do ADPLL

--- Python/PSD_NDCO.py
+++ Python/PSD_NDCO.py
@@ -5,6 +5,8 @@ Created on abril 01 21:23:40 2023
 
 @author: Ânderson Felipe Weschenfelder
 """
+from _ast import expr
+
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
@@ -40,30 +42,37 @@ def fun_calc_psd(x, fs=1, rbw=100e3, fstep=None):
     return XdB, f
 
 
-N = 4
-fs = 5e9
+N = 3
+fs = 450e6
 ts = 1 / fs
-f1 = 25e6
+f1 = 1.33e6
 V = 1
 t = np.arange(0, 400 * 1 / f1, ts)
+# resolução de cada nível. Para N = 3bits são 8 níveis, e se V=1V, cada nível vale 1/8V.,
 res_quantization = V * 2 / 2 ** N
 
 x_t = V * np.sin(2 * np.pi * f1 * t)
-x_n = np.round(x_t / res_quantization) * res_quantization
+# x_n = np.round(x_t / res_quantization) * res_quantization
+# divide o valor de cada amostra de x_n pela resolução de N bits, e pega o valor arredondado. Será gerado valores de 0 até 8 para N = 3
+x_n = np.round(x_t / res_quantization)
+# Do valor arredondado multiplica pela resolução para normalizar a amplitude V novamente.
+x_n = x_n * res_quantization
 
-# Calculando o erro de quantização
+# Calculando o erro
 err = x_t - x_n
+
+# Quanto mais níveis melhor a resolução e menor o erro de quantização
 
 # Plotando o sinal original, quantizado e o erro de quantização
 fig, axs = plt.subplots(3, 1, figsize=(10, 8))
 
-axs[0].plot(t, x_t)
+axs[0].plot(t[:1000], x_t[:1000])
 axs[0].set_title('Sinal Original')
 
-axs[1].step(t, x_n, where='post')
+axs[1].step(t[:1000], x_n[:1000], where='post')
 axs[1].set_title('Sinal Quantizado (3 bits)')
 
-axs[2].plot(t, err)
+axs[2].plot(t[:1000], err[:1000])
 axs[2].set_title('Erro de Quantização')
 
 plt.tight_layout()  # ajustar automaticamente as posições das subplots de um gráfico para que não haja sobreposição de legendas, rótulos de eixos ou títulos.
@@ -77,34 +86,43 @@ XdB_q, fq = fun_calc_psd((x_n), fs, 100e3)
 # plt.semilogy(f, x)
 
 plt.figure()
-plt.plot(fo / 1e6, XdB_o, label="full resolution")
+plt.plot(fo / 1e6, XdB_o, label="Full resolution")
 # plt.semilogy(fo/1e6, XdB_o,label="full resolution")
-plt.plot(fq / 1e6, XdB_q, label="full resolution")
+plt.plot(fq / 1e6, XdB_q, label="Quantized")
 # plt.semilogy(fq/1e6, XdB_q,label=""full resolution")
+plt.plot(fq / 1e6, np.full_like(fq,  np.mean(XdB_q)), 'black',label="mean")
 plt.grid(visible=True)
 plt.legend()
-plt.xlabel('Time (ns)')
+plt.xlabel('Feq (MHz)')
 plt.ylabel('Amplitude')
 plt.show()
 
 # Identificar a banda de frequência do sinal
-f_signal_min = f1 - 1  # margem esquerda da banda
-f_signal_max = f1 + 1  # margem direita da banda
+f_signal_min = f1 - f1/2    # margem esquerda da banda
+f_signal_max = f1 + f1/2   # margem direita da banda
 
 # Identificar a banda de frequência do ruído
-f_noise_min = 0  # margem esquerda da banda
-f_noise_max = f_signal_min - 1  # margem direita da banda
+f_noise_min = 0     # margem esquerda da banda
+f_noise_max = 100e3    # margem direita da banda
 
 # Calcular a PSD média do sinal na banda de interesse
 Pxx_signal = np.mean(XdB_q[(fq >= f_signal_min) & (fq <= f_signal_max)])
 
 # Calcular a potência do sinal na banda de interesse
 Ps = np.trapz(XdB_q[(fq >= f_signal_min) & (fq <= f_signal_max)], fq[(fq >= f_signal_min) & (fq <= f_signal_max)])
+# Ps = np.trapz(XdB_o, fo)
+print("ps:", Ps)
 
 # Calcular a potência do ruído fora da banda de interesse
 Pn = np.trapz(XdB_q[(fq >= f_noise_min) & (fq <= f_noise_max)], fq[(fq >= f_noise_min) & (fq <= f_noise_max)])
+# Pn = np.trapz(XdB_q, fq)
+print("pn:", Pn)
 
 # Calcular a SNR em dB
 SNR = 10 * np.log10(Ps / Pn)
 
-print('SNR: {:.2f} dB'.format(SNR))
+print('SNR: {:.2f} dBc / Hz'.format(SNR))
+
+SNR_Calc = -6.02 * N - 1.76 - 10 * np.log10(fs)
+
+print('SNR Teorico: {:.2f} dBc/ Hz'.format(SNR_Calc))
